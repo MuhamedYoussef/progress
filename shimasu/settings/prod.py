625e73c7614c6fb5f4cae3b0bb2468760a794d3a
Mohamed Youssef <muhamed-youssef@outlook.com>
2019-01-18 16:00:25 +0200

    added right credits

--- shimasu/settings/prod.py
+++ shimasu/settings/prod.py
@@ -4,13 +4,13 @@ DEBUG = False
 ADMINS = (
     ('Mohamed Youssef', 'mu7med.youssef@gmail.com'),
 )
-ALLOWED_HOSTS = ['techstor.herokuapp.com']
+ALLOWED_HOSTS = ['shimasu-progress.herokuapp.com']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dvgv8sgqkakrd',
-        'HOST': 'ec2-79-125-4-96.eu-west-1.compute.amazonaws.com',
-        'USER': 'uqndlrerapeuiz',
-        'PASSWORD': '0eea8c014a9519333d9a1b40ce93cbfa9e8291583f8787381bc66bbb0b3f6c28',
+        'NAME': 'd81mt03ukhmtf6',
+        'HOST': 'ec2-54-75-245-94.eu-west-1.compute.amazonaws.com',
+        'USER': 'ihyqfibforfslm',
+        'PASSWORD': '73d2b66759068ec0e8e3b4f41e43dd1293798ed9fe9e2ea9225e8ee2e125c201',
     }
 }
